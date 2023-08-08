b490a6573e4da7432d07b21b8bf7421e376e411a
eladgat <eladgat@mail.tau.ac.il>
2023-08-08 16:07:02 +0300

    set no balanced data as an arg

--- utils.py
+++ utils.py
@@ -127,7 +127,7 @@ def save_generations(generation, args, generation_type):
 def get_generation_file_name(args, generation_type):
     arg_dict = vars(args)
     exclude_keys = ["save_dir", "cache_dir", "device", "true_label", "threshold", "parallelize", "split",
-                    "use_decoder", "all_layers"]
+                    "use_decoder", "all_layers", "no_data_balance"]
     filename = generation_type + "__" + "__".join(['{}_{}'.format(k, v) for k, v in arg_dict.items()
                                                    if k not in exclude_keys]) + ".npy".format(generation_type)
     return filename
